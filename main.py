246308f9a8dd3478f1f92e60950a48082fdb06e8
paxington181 <tsandrus@rogers.com>
2026-05-03 18:10:18 -0400

    Building lists

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+
+species: str = ["aasimar", "dragonborn", "dwarf", "elf", "gnome", "goliath", "halfling", "human", "orc", "tiefling"]
+classes: str = ["artificer", "barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
+background: str = ["acolyte", "artisan", "charlatan", "criminal", "entertainer", "farmer", "guard", "guide", "hermit", "merchant", "noble", "sage", "sailor", "scribe", "soldier", "wayfarer"]
