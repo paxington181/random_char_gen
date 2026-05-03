4f4156fd5ac922b9f7ce61375628a529a4fa6281
paxington181 <tsandrus@rogers.com>
2026-05-03 19:03:55 -0400

    More data entry, reorganized for ease of adding more

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import random
 
-species: str = ["aasimar", "dragonborn", "dwarf", "elf", "gnome", "goliath", "halfling", "human", "orc", "tiefling"]
-classes: str = ["artificer", "barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
-background: str = ["acolyte", "artisan", "charlatan", "criminal", "entertainer", "farmer", "guard", "guide", "hermit", "merchant", "noble", "sage", "sailor", "scribe", "soldier", "wayfarer"]
+base_species: str = ["aasimar", "dragonborn", "dwarf", "elf", "gnome", "goliath", "halfling", "human", "orc", "tiefling"]
+eberron_species: str = []
+mythozoology_species: str = ["hoofborn", "inukin"]
+
+
 dragonborn_sub: str = ["black", "blue", "brass", "bronze", "copper", "gold", "green", "red", "silver", "white"]
 elf_sub: str = ["drow", "high elf", "wood elf"]
 gnome_sub: str = ["forest gnome", "rock gnome"]
 goliath_sub: str = ["cloud", "fire", "frost", "hill", "stone", "storm"]
 tiefling_sub: str = ["abyssal", "chthonic", "infernal"]
+hoofborn_sub: str = ["tidehoof"]
+
+
+base_classes: str = ["barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
+eberron_classes: str = ["artificer"]
+
+
+base_background: str = ["acolyte", "artisan", "charlatan", "criminal", "entertainer", "farmer", "guard", "guide", "hermit", "merchant", "noble", "sage", "sailor", "scribe", "soldier", "wayfarer"]
+
