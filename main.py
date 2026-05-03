c817bead7eec81e783b8c1a73c36f155e83731c5
paxington181 <tsandrus@rogers.com>
2026-05-03 18:25:37 -0400

    data entry

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import random
 species: str = ["aasimar", "dragonborn", "dwarf", "elf", "gnome", "goliath", "halfling", "human", "orc", "tiefling"]
 classes: str = ["artificer", "barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
 background: str = ["acolyte", "artisan", "charlatan", "criminal", "entertainer", "farmer", "guard", "guide", "hermit", "merchant", "noble", "sage", "sailor", "scribe", "soldier", "wayfarer"]
+dragonborn_sub: str = ["black", "blue", "brass", "bronze", "copper", "gold", "green", "red", "silver", "white"]
+elf_sub: str = ["drow", "high elf", "wood elf"]
+gnome_sub: str = ["forest gnome", "rock gnome"]
+goliath_sub: str = ["cloud", "fire", "frost", "hill", "stone", "storm"]
+tiefling_sub: str = ["abyssal", "chthonic", "infernal"]
