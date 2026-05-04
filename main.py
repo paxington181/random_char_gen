ef210e7f20bad1186e8fc132fb72184acb43d765
paxington181 <tsandrus@rogers.com>
2026-05-04 18:27:10 -0400

    Basic random generation set up

--- main.py
+++ main.py
@@ -19,3 +19,22 @@ eberron_classes: str = ["artificer"]
 
 base_background: str = ["acolyte", "artisan", "charlatan", "criminal", "entertainer", "farmer", "guard", "guide", "hermit", "merchant", "noble", "sage", "sailor", "scribe", "soldier", "wayfarer"]
 eberron_background: str = ["aberrant heir", "archaeologist", "house agent", "house cannith heir", "house deneith heir", "house ghallanda heir", "house jorasco heir", "house kundarak heir", "house lyrandar heir", "house medani heir", "house orien heir", "house phiarlan heir", "house sivis heir", "house tharashk heir", "house vadalis heir", "inquisitive"]
+
+
+
+def main():
+    
+    selected_species = base_species + eberron_species + mythozoology_species
+    random_species = selected_species[(random.randrange(0, (len(selected_species) - 1 )))]
+    print(random_species)
+
+
+    selected_classes = base_classes + eberron_classes
+    random_class = selected_classes[random.randrange(0, (len(selected_classes) - 1))]
+    print(random_class)
+    
+    selected_background = base_background + eberron_background
+    random_background = selected_background[random.randrange(0, (len(selected_background) - 1))]
+    print(random_background)
+
+main()
