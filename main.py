8287996974a7c9cac5c39f49f623bfeb3dee69a1
paxington181 <tsandrus@rogers.com>
2026-05-03 21:54:38 -0400

    Time to write actual code, the mess of strings is in

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
 base_species: str = ["aasimar", "dragonborn", "dwarf", "elf", "gnome", "goliath", "halfling", "human", "orc", "tiefling"]
-eberron_species: str = []
-mythozoology_species: str = ["hoofborn", "inukin"]
+eberron_species: str = ["changling", "kalashtar", "khoravar", "shifter", "warforged"]
+mythozoology_species: str = ["hoofborn", "inukin", "kappa"]
 
 
 dragonborn_sub: str = ["black", "blue", "brass", "bronze", "copper", "gold", "green", "red", "silver", "white"]
@@ -11,11 +11,11 @@ gnome_sub: str = ["forest gnome", "rock gnome"]
 goliath_sub: str = ["cloud", "fire", "frost", "hill", "stone", "storm"]
 tiefling_sub: str = ["abyssal", "chthonic", "infernal"]
 hoofborn_sub: str = ["tidehoof"]
-
+kappa_sub: str = ["wrangler", "tideweaver"]
 
 base_classes: str = ["barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
 eberron_classes: str = ["artificer"]
 
 
 base_background: str = ["acolyte", "artisan", "charlatan", "criminal", "entertainer", "farmer", "guard", "guide", "hermit", "merchant", "noble", "sage", "sailor", "scribe", "soldier", "wayfarer"]
-
+eberron_background: str = ["aberrant heir", "archaeologist", "house agent", "house cannith heir", "house deneith heir", "house ghallanda heir", "house jorasco heir", "house kundarak heir", "house lyrandar heir", "house medani heir", "house orien heir", "house phiarlan heir", "house sivis heir", "house tharashk heir", "house vadalis heir", "inquisitive"]
