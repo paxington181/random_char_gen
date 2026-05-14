85f24db0ff9e477c3ce5a322568960881010b068
paxington181 <tsandrus@rogers.com>
2026-05-14 09:02:44 -0400

    Refactored rolling 6 3d6 stats

--- stat_gen.py
+++ stat_gen.py
@@ -43,18 +43,19 @@ def tdsix_roll():
     return tds_sum
 
 def tdssix_stats():
-    stats_sum: int = []
+    tdssix_list: int = []
     for i in range(0, 6):
-        stats_sum.append(tdsix_roll())
+        tdssix_list.append(tdsix_roll())
         i += 1
 
-    print(stats_sum)
-    sum_stats: int = sum(stats_sum)
-    sum_rounded: float = round((sum_stats / 72), 2)
-        
-
-    print(f"The sum of your stats: {sum(stats_sum)}")
-    print(f"The sum of rolled stats are {int(sum_rounded * 100)}% of the sum of the standard array.")
+    tdssix_sorted: int = sorted(tdssix_list)    
+    tdssix_flipped: int = tdssix_sorted[::-1]
+    tdssix_sum: int = sum(tdssix_list)
+    tdssix_rounded: float = round((tdssix_sum / 72), 2)
+    
+    print(f"Your stats are {tdssix_flipped}")
+    print(f"The sum of your stats: {tdssix_sum}")
+    print(f"The sum of rolled stats are {int(tdssix_rounded * 100)}% of the sum of the standard array.")
 
 
 def main():
