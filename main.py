799fc2d5ecd9d566169734367998966121687366
paxington181 <tsandrus@rogers.com>
2026-05-19 11:48:08 -0400

    Renamed test since I started writing code in it, made new test.

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import tkinter as tk
+from tkinter import ttk
+
+
+
+main = tk.Tk()
+
+
+ttk.Label(main, text = 'Press Me').pack()
+
+main.mainloop()
--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 import random
-import tkinter as tk
-from tkinter import ttk
 
-
-
-maincycle = tk.Tk()
-
-ttk.Label(maincycle, text = 'Press Me').pack()
-
-maincycle.mainloop()
+def main():
+    pass
+main()
