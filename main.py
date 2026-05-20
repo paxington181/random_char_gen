d880f8039dba24f770bf69e10403ab4434591574
paxington181 <tsandrus@rogers.com>
2026-05-20 08:38:53 -0400

    Front end programmers are wizards.

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from tkinter import ttk
 
 
 main = tk.Tk()
+main.title("D&D Character Randomizer")
+main.configure(background="#5d4340")
+main.minsize(300, 300)
+main.maxsize(500, 500)
+main.geometry("400x400+100+100")
 
+ttk.Label(main, text = 'UIs are a headache').pack()
 
-ttk.Label(main, text = 'Press Me').pack()
 
 main.mainloop()
