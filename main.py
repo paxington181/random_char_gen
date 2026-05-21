6abf34f420bc379f71e0bae66e75ee1086a9c715
paxington181 <tsandrus@rogers.com>
2026-05-21 08:38:22 -0400

    Starting alterating to make the player stats work

--- main.py
+++ main.py
@@ -11,25 +11,41 @@ main.minsize(300, 300)
 main.maxsize(1000, 500)
 main.geometry("700x400+100+100")
 
+
 ttk.Label(main, text = "Races/Species").grid(row = 0, column = 0)
 ttk.Label(main, text = "Class").grid(row = 0, column = 1)
 ttk.Label(main, text = "Background").grid(row = 0, column = 3)
 ttk.Label(main, text = "'SubRaces/Species'").grid(row = 1, column = 0)
 ttk.Label(main, text = "'Sub Class'").grid(row = 1, column = 1)
+
 ttk.Label(main, text = "Dice Rolling Method").grid(row = 0, column = 4)
+
 ttk.Label(main, text = "Strength").grid(row = 2, column = 0)
-ttk.Label(main, text = "Strength number").grid(row = 2, column = 1)
+str_entry:int = tk.IntVar()
+ttk.Entry(main, textvariable = str_entry).grid(row = 2, column = 1)
+
 ttk.Label(main, text = "Dexterity").grid(row = 3, column = 0)
-ttk.Label(main, text = "Dexterity number").grid(row = 3, column = 1)
+dex_entry:int  = tk.IntVar()
+ttk.Entry(main, textvariable = dex_entry).grid(row = 3, column = 1)
+
 ttk.Label(main, text = "Constitution").grid(row = 4, column = 0)
-ttk.Label(main, text = "Constitution number").grid(row = 4, column = 1)
+con_entry:int = tk.IntVar()
+ttk.Entry(main, textvariable = con_entry).grid(row = 4, column = 1)
+
 ttk.Label(main, text = "Intellegence").grid(row = 5, column = 0)
-ttk.Label(main, text = "Intellegence number").grid(row = 5, column = 1)
+int_entry:int = tk.IntVar()
+ttk.Entry(main, textvariable = int_entry).grid(row = 5, column = 1)
+
 ttk.Label(main, text = "Wisdom").grid(row = 6, column = 0)
-ttk.Label(main, text = "Wisdom number").grid(row = 6, column = 1)
+wis_entry:int = tk.IntVar()
+ttk.Entry(main, textvariable = wis_entry).grid(row = 6, column = 1)
+
 ttk.Label(main, text = "Charisma").grid(row = 7, column = 0)
-ttk.Label(main, text = "Charisma number").grid(row = 7, column = 1)
+cha_entry:int = tk.IntVar()
+ttk.Entry(main, textvariable = cha_entry).grid(row = 7, column = 1)
+
 ttk.Label(main, text = "Picture placeholder").grid(row = 2, column = 3)
+
 ttk.Label(main, text = "Randomize").grid(row = 8, column = 4)
 ttk.Label(main, text = "Reset").grid(row = 8, column = 5)
 
