a812f7a9f9fe1d5bcc19668a1eb06ff816164028
paxington181 <tsandrus@rogers.com>
2026-06-02 09:38:13 -0400

    Just need to add dice rolling to the UI.  Oh that's clean so far.

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from tkinter import ttk
 #main window organization
 main = tk.Tk()
 main.title("D&D Character Randomizer")
-main.geometry('1000x400')
+main.geometry('900x400')
 
 #geometry control
 
-main.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9), uniform = "a")
-main.columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), weight = 1, uniform = "a")
+main.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), uniform = "a")
+main.columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), weight = 1, uniform = "a")
 
 #widget creation
 
@@ -37,7 +37,7 @@ stat_gen_method = ttk.Label(main, text = "statg")
 stats_to_assign = ttk.Label(main, text = "statta")
 
 
-str_label = ttk.Label(main, text = "Str", background = "brown")
+str_label = ttk.Label(main, text = "Str")
 str_stat = ttk.Entry(main)
 str_modifier = ttk.Label(main, text = "+0")
 
@@ -64,29 +64,51 @@ cha_modifier = ttk.Label(main, text = "+0")
 image_placeholder = ttk.Label(main, text = "Replace with Character Pixel Art")
 
 randomize_button = ttk.Button(main, text = "Randomize")
-reset_button = ttk.Button(main, text = "Reset all options")
+reset_button = ttk.Button(main, text = "Reset Options")
 
 #widget placement
 
-class_randomized.grid(row = 0, column = 0, columnspan=3, sticky = "nwe")
-class_label.grid(row = 1, column = 0, columnspan=3, sticky = "nwe")
-class_random_selection.grid(row = 2, column = 0, columnspan=3, sticky = "nwe")
+class_randomized.grid(row = 0, column = 0, columnspan = 3, sticky = "nwe")
+class_label.grid(row = 1, column = 0, columnspan = 3, sticky = "nwe")
+class_random_selection.grid(row = 2, column = 0, columnspan = 3, sticky = "nwe")
 
-subclass_randomized.grid(row = 0, column = 3, columnspan=3, sticky = "nwe")
-subclass_label.grid(row = 1, column = 3, columnspan=3, sticky = "nwe")
+subclass_randomized.grid(row = 0, column = 3, columnspan = 3, sticky = "nwe")
+subclass_label.grid(row = 1, column = 3, columnspan = 3, sticky = "nwe")
 subclass_random_selection.grid(row = 2, column = 3, columnspan=3, sticky = "nwe")
 
-species_randomized.grid(row = 0, column = 6, columnspan=3, sticky = "nwe")
-species_label.grid(row = 1, column = 6, columnspan=3, sticky = "nwe")
-species_random_selection.grid(row = 2, column = 6, columnspan=3, sticky = "nwe")
+species_randomized.grid(row = 0, column = 6, columnspan = 3, sticky = "nwe")
+species_label.grid(row = 1, column = 6, columnspan = 3, sticky = "nwe")
+species_random_selection.grid(row = 2, column = 6, columnspan = 3, sticky = "nwe")
 
-background_randomized.grid(row = 0, column = 9, columnspan=3, sticky = "nwe")
-background_label.grid(row = 1, column = 9, columnspan=3, sticky = "nwe")
-background_random_selection.grid(row = 2, column = 9, columnspan=3, sticky = "nwe")
+background_randomized.grid(row = 0, column = 9, columnspan = 3, sticky = "nwe")
+background_label.grid(row = 1, column = 9, columnspan = 3, sticky = "nwe")
+background_random_selection.grid(row = 2, column = 9, columnspan = 3, sticky = "nwe")
 
 str_label.grid(row = 4, column = 0, sticky = "nwe")
 str_stat.grid(row = 4, column = 1, sticky = "nwe")
 str_modifier.grid(row = 4, column = 2, sticky = "nwe")
 
+dex_label.grid(row = 5, column = 0, sticky = "nwe")
+dex_stat.grid(row = 5, column = 1, sticky = "nwe")
+dex_modifier.grid(row = 5, column = 2, sticky = "nwe")
+
+con_label.grid(row = 6, column = 0, sticky = "nwe")
+con_stat.grid(row = 6, column = 1, sticky = "nwe")
+con_modifier.grid(row = 6, column = 2, sticky = "nwe")
+
+int_label.grid(row = 7, column = 0, sticky = "nwe")
+int_stat.grid(row = 7, column = 1, sticky = "nwe")
+int_modifier.grid(row = 7, column = 2, sticky = "nwe")
+
+wis_label.grid(row = 8, column = 0, sticky = "nwe")
+wis_stat.grid(row = 8, column = 1, sticky = "nwe")
+wis_modifier.grid(row = 8, column = 2, sticky = "nwe")
+
+cha_label.grid(row = 9, column = 0, sticky = "nwe")
+cha_stat.grid(row = 9, column = 1, sticky = "nwe")
+cha_modifier.grid(row = 9, column = 2, sticky = "nwe")
+
+randomize_button.grid(row = 10, column = 8,  columnspan = 2, sticky = "nwe")
+reset_button.grid(row = 10, column = 10,  columnspan = 2, sticky = "nwe")
 
 main.mainloop()
