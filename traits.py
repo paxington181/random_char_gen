693f61d986783fdd156cc618fa30960e03404c9f
paxington181 <tsandrus@rogers.com>
2026-06-16 09:35:54 -0400

    Minor work on traits.py

--- traits.py
+++ traits.py
@@ -1,2 +1,7 @@
 import random
 
+alignment: list[str] = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"]
+inspirations: list[str] = []
+goal: list[str] = []
+family: list[str] = []
+
