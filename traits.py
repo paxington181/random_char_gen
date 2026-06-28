51065fb3388c53a3639eeb7ce0c9c80d08a8dc4f
paxington181 <tsandrus@rogers.com>
2026-06-28 11:03:16 -0400

    Filling out more of traits.py

--- traits.py
+++ traits.py
@@ -3,9 +3,9 @@ import random
 alignment: list[str] = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"]
 ideal: list[str] = []
 personality: list[str] = []
-bond: list[str] = []
-flaw: list[str] = []
-family: list[str] = []
+bond: list[str] = ["self", "familial", "organizational", "community", "region", "country"]
+flaw: list[str] = ["prideful", "greedy", "wrathful", "envious", "lustful", "gluttonous", "slothful"]
+family: list[str] = ["child", "children", "sister", "brother", "multiple siblings", ]
 mentor: list[str] = ["relative", "wandering stranger", "school", ""]
 mentor_attitude: list[str] = ["obliging", "friendly", "neutral", "hostile", "enemy"]
 
