516464c289c875e675aaad9798fba4293de37893
paxington181 <tsandrus@rogers.com>
2026-06-28 13:15:49 -0400

    traits.py fleshed out more

--- traits.py
+++ traits.py
@@ -1,13 +1,22 @@
 import random
 
-alignment: list[str] = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"]
+align_law_chaos: list[str] = ["lawful", "neutral", "chaotic"]
+align_good_evil: list[str] = ["good", "neutral", "evil"]
+align_chaos: list[str] = ["boastful", "impulsive", "rebellious", "self-absorbed"]
+align_good: list[str] = ["compassionate", "helpful", "honest", "kind"]
+align_evil: list[str] = ["dishonest", "vengeful", "cruel", "greedy"]
+align_lawful: list[str] = ["cooperative", "loyal", "judgemental", "methodical"]
+align_neutral: list[str] = ["selfish", "disinterested", "laconic", "pragmatic"]
+
 ideal: list[str] = []
-personality: list[str] = []
+personality: list[str] = ["alignment trait to fault"]
 bond: list[str] = ["self", "familial", "organizational", "community", "region", "country"]
 flaw: list[str] = ["prideful", "greedy", "wrathful", "envious", "lustful", "gluttonous", "slothful"]
+
 family: list[str] = ["child", "children", "sister", "brother", "multiple siblings", ]
 mentor: list[str] = ["relative", "wandering stranger", "school", ""]
 mentor_attitude: list[str] = ["friendly", "neutral", "hostile"]
+
 
 tier1_events: list[str] = []
 tier2_events: list[str] = []
