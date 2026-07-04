dc81f9977a7ef044406d087250ee992d05e722f2
paxington181 <tsandrus@rogers.com>
2026-07-04 18:39:09 -0400

    Buttons removed, figured out my bug but radio buttons make more sense and are less ugly.

--- main.py
+++ main.py
@@ -21,30 +21,7 @@ def modifier_update(current_stat):
         return f"{mod}"
     return "+0"
 
-def str_update(current_stat):
-    str_stat.configure(text = f"{current_stat}")
-    print(current_stat)
-    str_modifier.configure(text = f"{modifier_update(current_stat)}")
 
-def dex_update(current_stat):
-    dex_stat.configure(text = f"{current_stat}")
-    dex_modifier.configure(text = f"{modifier_update(current_stat)}")
-
-def con_update(current_stat):
-    con_stat.configure(text = f"{current_stat}")
-    con_modifier.configure(text = f"{modifier_update(current_stat)}")
-
-def int_update(current_stat):
-    int_stat.configure(text = f"{current_stat}")
-    int_modifier.configure(text = f"{modifier_update(current_stat)}")
-
-def wis_update(current_stat):
-    wis_stat.configure(text = f"{current_stat}")
-    wis_modifier.configure(text = f"{modifier_update(current_stat)}")
-
-def cha_update(current_stat):
-    cha_stat.configure(text = f"{current_stat}")
-    cha_modifier.configure(text = f"{modifier_update(current_stat)}")
 
 def stat_buttons():
     pass
@@ -75,27 +52,6 @@ def stats_update(current_stats):
     stats_percent.configure(text = f" {int(round((current_stats[6] / 72), 2) * 100)}%")
     #mass_stat_button(current_stats[0][0], current_stats[1][0], current_stats[2][0], current_stats[3][0], current_stats[4][0], current_stats[5][0])
 
-def hc_stat_update(current_stats):
-    str_update(current_stats[0][0])
-    print(current_stats[0][0])
-    dex_update(current_stats[1][0])
-    con_update(current_stats[2][0])
-    int_update(current_stats[3][0])
-    wis_update(current_stats[4][0])
-    cha_update(current_stats[5][0])
-    #mass_stat_button("", "", "", "", "", "")
-
-
-def reset_stats():
-    str_update(10)
-    dex_update(10)
-    con_update(10)
-    int_update(10)
-    wis_update(10)
-    cha_update(10)
-    stats_gen_selector.set("Standard")
-    #mass_stat_button("", "", "", "", "", "")
-
 def roll_stats_standard():
     current_stats = [15, 14, 13, 12, 10, 8]
     stats_rolls_one.configure(text = f"{current_stats[0]}")
@@ -112,7 +68,6 @@ def roll_stats_standard():
     stats_sum_six.configure(text = f"{current_stats[5]}")
     stats_total.configure(text = f" {sum(current_stats)}")
     stats_percent.configure(text = f" {int(round((sum(current_stats) / 72), 2) * 100)}%")
-    mass_stat_button(15, 14, 13, 12, 10, 8)
 
 def roll_stats_tdsix():
     current_stats = tdsix_set()
@@ -133,73 +88,18 @@ def roll_stats_mdsix_shuffle():
 def roll_stats_hc_tdsix():
     current_stats = hc_tdsix_set()
     stats_update(current_stats)
-    hc_stat_update(current_stats)
+    #hc_stat_update(current_stats)
 
 def roll_stats_hc_fdsix():
     current_stats = hc_fdsix_set()
     stats_update(current_stats)
-    hc_stat_update(current_stats)
+    #hc_stat_update(current_stats)
 
 def roll_stats_hc_mdsix():
     current_stats = hc_mdsix_set()
     stats_update(current_stats)
-    hc_stat_update(current_stats)
+    #hc_stat_update(current_stats)
 
-def str_button_config(value1, value2, value3, value4, value5, value6):
-    str_button_1.configure(text = f"{(value1)}", command = str_update(value1))
-    str_button_2.configure(text = f"{(value2)}", command = str_update(value2))
-    str_button_3.configure(text = f"{(value3)}", command = str_update(value3))
-    str_button_4.configure(text = f"{(value4)}", command = str_update(value4))
-    str_button_5.configure(text = f"{(value5)}", command = str_update(value5))
-    str_button_6.configure(text = f"{(value6)}", command = str_update(value6))
-
-def dex_button_config(value1, value2, value3, value4, value5, value6):
-    dex_button_1.configure(text = f"{(value1)}", command = dex_update(value1))
-    dex_button_2.configure(text = f"{(value2)}", command = dex_update(value2))
-    dex_button_3.configure(text = f"{(value3)}", command = dex_update(value3))
-    dex_button_4.configure(text = f"{(value4)}", command = dex_update(value4))
-    dex_button_5.configure(text = f"{(value5)}", command = dex_update(value5))
-    dex_button_6.configure(text = f"{(value6)}", command = dex_update(value6))
-
-def con_button_config(value1, value2, value3, value4, value5, value6):
-    con_button_1.configure(text = f"{(value1)}", command = con_update(value1))
-    con_button_2.configure(text = f"{(value2)}", command = con_update(value2))
-    con_button_3.configure(text = f"{(value3)}", command = con_update(value3))
-    con_button_4.configure(text = f"{(value4)}", command = con_update(value4))
-    con_button_5.configure(text = f"{(value5)}", command = con_update(value5))
-    con_button_6.configure(text = f"{(value6)}", command = con_update(value6))
-
-def int_button_config(value1, value2, value3, value4, value5, value6):
-    int_button_1.configure(text = f"{(value1)}", command = int_update(value1))
-    int_button_2.configure(text = f"{(value2)}", command = int_update(value2))
-    int_button_3.configure(text = f"{(value3)}", command = int_update(value3))
-    int_button_4.configure(text = f"{(value4)}", command = int_update(value4))
-    int_button_5.configure(text = f"{(value5)}", command = int_update(value5))
-    int_button_6.configure(text = f"{(value6)}", command = int_update(value6))
-
-def wis_button_config(value1, value2, value3, value4, value5, value6):
-    wis_button_1.configure(text = f"{(value1)}", command = wis_update(value1))
-    wis_button_2.configure(text = f"{(value2)}", command = wis_update(value2))
-    wis_button_3.configure(text = f"{(value3)}", command = wis_update(value3))
-    wis_button_4.configure(text = f"{(value4)}", command = wis_update(value4))
-    wis_button_5.configure(text = f"{(value5)}", command = wis_update(value5))
-    wis_button_6.configure(text = f"{(value6)}", command = wis_update(value6))
-
-def cha_button_config(value1, value2, value3, value4, value5, value6):
-    cha_button_1.configure(text = f"{(value1)}", command = cha_update(value1))
-    cha_button_2.configure(text = f"{(value2)}", command = cha_update(value2))
-    cha_button_3.configure(text = f"{(value3)}", command = cha_update(value3))
-    cha_button_4.configure(text = f"{(value4)}", command = cha_update(value4))
-    cha_button_5.configure(text = f"{(value5)}", command = cha_update(value5))
-    cha_button_6.configure(text = f"{(value6)}", command = cha_update(value6))
-
-def mass_stat_button(value1, value2, value3, value4, value5, value6):
-    str_button_config(value1, value2, value3, value4, value5, value6)
-    dex_button_config(value1, value2, value3, value4, value5, value6)
-    con_button_config(value1, value2, value3, value4, value5, value6)
-    int_button_config(value1, value2, value3, value4, value5, value6)
-    wis_button_config(value1, value2, value3, value4, value5, value6)
-    cha_button_config(value1, value2, value3, value4, value5, value6)
 
 def roll_method_change(event):
     selection = stats_gen_selector.get()
@@ -280,64 +180,34 @@ stats_percent_label = ttk.Label(main, text = "of standard set", anchor = "w", ba
 str_label = ttk.Label(main, text = "Str", anchor = "e", background = background_color, foreground = foreground_color)
 str_stat = ttk.Label(main, text = 10, anchor = "center", background = background_color, foreground = foreground_color)
 str_modifier = ttk.Label(main, text = "+0", background = background_color, foreground = foreground_color)
-str_button_1 = ttk.Button(main, text = "", command = stat_buttons)
-str_button_2 = ttk.Button(main, text = "")
-str_button_3 = ttk.Button(main, text = "")
-str_button_4 = ttk.Button(main, text = "")
-str_button_5 = ttk.Button(main, text = "")
-str_button_6 = ttk.Button(main, text = "")
+
 
 dex_label = ttk.Label(main, text = "Dex", anchor = "e", background = background_color, foreground = foreground_color)
 dex_stat = ttk.Label(main, text = 10, anchor = "center", background = background_color, foreground = foreground_color)
 dex_modifier = ttk.Label(main, text = "+0", background = background_color, foreground = foreground_color)
-dex_button_1 = ttk.Button(main, text = "")
-dex_button_2 = ttk.Button(main, text = "")
-dex_button_3 = ttk.Button(main, text = "")
-dex_button_4 = ttk.Button(main, text = "")
-dex_button_5 = ttk.Button(main, text = "")
-dex_button_6 = ttk.Button(main, text = "")
+
 
 con_label = ttk.Label(main, text = "Con", anchor = "e", background = background_color, foreground = foreground_color)
 con_stat = ttk.Label(main, text = 10, anchor = "center", background = background_color, foreground = foreground_color)
 con_modifier = ttk.Label(main, text = "+0", background = background_color, foreground = foreground_color)
-con_button_1 = ttk.Button(main, text = "")
-con_button_2 = ttk.Button(main, text = "")
-con_button_3 = ttk.Button(main, text = "")
-con_button_4 = ttk.Button(main, text = "")
-con_button_5 = ttk.Button(main, text = "")
-con_button_6 = ttk.Button(main, text = "")
+
 
 int_label = ttk.Label(main, text = "Int", anchor = "e", background = background_color, foreground = foreground_color)
 int_stat = ttk.Label(main, text = 10, anchor = "center", background = background_color, foreground = foreground_color)
 int_modifier = ttk.Label(main, text = "+0", background = background_color, foreground = foreground_color)
-int_button_1 = ttk.Button(main, text = "")
-int_button_2 = ttk.Button(main, text = "")
-int_button_3 = ttk.Button(main, text = "")
-int_button_4 = ttk.Button(main, text = "")
-int_button_5 = ttk.Button(main, text = "")
-int_button_6 = ttk.Button(main, text = "")
+
 
 wis_label = ttk.Label(main, text = "Wis", anchor = "e", background = background_color, foreground = foreground_color)
 wis_stat = ttk.Label(main, text = 10, anchor = "center", background = background_color, foreground = foreground_color)
 wis_modifier = ttk.Label(main, text = "+0", background = background_color, foreground = foreground_color)
-wis_button_1 = ttk.Button(main, text = "")
-wis_button_2 = ttk.Button(main, text = "")
-wis_button_3 = ttk.Button(main, text = "")
-wis_button_4 = ttk.Button(main, text = "")
-wis_button_5 = ttk.Button(main, text = "")
-wis_button_6 = ttk.Button(main, text = "")
+
 
 cha_label = ttk.Label(main, text = "Cha", anchor = "e", background = background_color, foreground = foreground_color)
 cha_stat = ttk.Label(main, text = 10, anchor = "center", background = background_color, foreground = foreground_color)
 cha_modifier = ttk.Label(main, text = "+0", background = background_color, foreground = foreground_color)
-cha_button_1 = ttk.Button(main, text = "")
-cha_button_2 = ttk.Button(main, text = "")
-cha_button_3 = ttk.Button(main, text = "")
-cha_button_4 = ttk.Button(main, text = "")
-cha_button_5 = ttk.Button(main, text = "")
-cha_button_6 = ttk.Button(main, text = "")
 
-stat_reset = tk.Button(main, text = "Reset Stats", command = reset_stats)
+
+stat_reset = tk.Button(main, text = "Reset Stats", command = None)
 randomize_button = ttk.Button(main, text = "Randomize", command = update_all)
 roll_stats_button = ttk.Button(main, text = "Roll Stats", command = roll_stats_standard)
 
@@ -358,62 +228,32 @@ background_label.grid(row = 1, column = 9, columnspan = 3, sticky = "nwe")
 str_label.grid(row = 2, column = 0, sticky = "nwe")
 str_stat.grid(row = 2, column = 1, sticky = "nwe")
 str_modifier.grid(row = 2, column = 2, sticky = "nwe")
-str_button_1.grid(row = 2, column = 3, sticky = "nswe")
-str_button_2.grid(row = 2, column = 4, sticky = "nswe")
-str_button_3.grid(row = 2, column = 5, sticky = "nswe")
-str_button_4.grid(row = 2, column = 6, sticky = "nswe")
-str_button_5.grid(row = 2, column = 7, sticky = "nswe")
-str_button_6.grid(row = 2, column = 8, sticky = "nswe")
+
 
 dex_label.grid(row = 3, column = 0, sticky = "nwe")
 dex_stat.grid(row = 3, column = 1, sticky = "nwe")
 dex_modifier.grid(row = 3, column = 2, sticky = "nwe")
-dex_button_1.grid(row = 3, column = 3, sticky = "nswe")
-dex_button_2.grid(row = 3, column = 4, sticky = "nswe")
-dex_button_3.grid(row = 3, column = 5, sticky = "nswe")
-dex_button_4.grid(row = 3, column = 6, sticky = "nswe")
-dex_button_5.grid(row = 3, column = 7, sticky = "nswe")
-dex_button_6.grid(row = 3, column = 8, sticky = "nswe")
+
 
 con_label.grid(row = 4, column = 0, sticky = "nwe")
 con_stat.grid(row = 4, column = 1, sticky = "nwe")
 con_modifier.grid(row = 4, column = 2, sticky = "nwe")
-con_button_1.grid(row = 4, column = 3, sticky = "nswe")
-con_button_2.grid(row = 4, column = 4, sticky = "nswe")
-con_button_3.grid(row = 4, column = 5, sticky = "nswe")
-con_button_4.grid(row = 4, column = 6, sticky = "nswe")
-con_button_5.grid(row = 4, column = 7, sticky = "nswe")
-con_button_6.grid(row = 4, column = 8, sticky = "nswe")
+
 
 int_label.grid(row = 5, column = 0, sticky = "nwe")
 int_stat.grid(row = 5, column = 1, sticky = "nwe")
 int_modifier.grid(row = 5, column = 2, sticky = "nwe")
-int_button_1.grid(row = 5, column = 3, sticky = "nswe")
-int_button_2.grid(row = 5, column = 4, sticky = "nswe")
-int_button_3.grid(row = 5, column = 5, sticky = "nswe")
-int_button_4.grid(row = 5, column = 6, sticky = "nswe")
-int_button_5.grid(row = 5, column = 7, sticky = "nswe")
-int_button_6.grid(row = 5, column = 8, sticky = "nswe")
+
 
 wis_label.grid(row = 6, column = 0, sticky = "nwe")
 wis_stat.grid(row = 6, column = 1, sticky = "nwe")
 wis_modifier.grid(row = 6, column = 2, sticky = "nwe")
-wis_button_1.grid(row = 6, column = 3, sticky = "nswe")
-wis_button_2.grid(row = 6, column = 4, sticky = "nswe")
-wis_button_3.grid(row = 6, column = 5, sticky = "nswe")
-wis_button_4.grid(row = 6, column = 6, sticky = "nswe")
-wis_button_5.grid(row = 6, column = 7, sticky = "nswe")
-wis_button_6.grid(row = 6, column = 8, sticky = "nswe")
+
 
 cha_label.grid(row = 7, column = 0, sticky = "nwe")
 cha_stat.grid(row = 7, column = 1, sticky = "nwe")
 cha_modifier.grid(row = 7, column = 2, sticky = "nwe")
-cha_button_1.grid(row = 7, column = 3, sticky = "nswe")
-cha_button_2.grid(row = 7, column = 4, sticky = "nswe")
-cha_button_3.grid(row = 7, column = 5, sticky = "nswe")
-cha_button_4.grid(row = 7, column = 6, sticky = "nswe")
-cha_button_5.grid(row = 7, column = 7, sticky = "nswe")
-cha_button_6.grid(row = 7, column = 8, sticky = "nswe")
+
 
 stats_gen_label.grid(row = 9, column = 0, columnspan = 2, sticky = "nswe")
 stats_gen_selector.grid(row = 9, column = 2, columnspan = 2, sticky = "nswe")
