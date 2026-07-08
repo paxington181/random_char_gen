4b2a91d9c3a36570d11a7d376f58744f803f09c7
paxington181 <tsandrus@rogers.com>
2026-07-08 12:35:58 -0400

    Minor updates to traits.py

--- traits.py
+++ traits.py
@@ -1,20 +1,21 @@
 import random
 
 align_law_chaos: list[str] = ["lawful", "neutral", "chaotic"]
+lc_weight: list[int] = [30, 30, 30]
 align_good_evil: list[str] = ["good", "neutral", "evil"]
+ge_weight: list[str] = [50, 50, 30]
 align_chaos: list[str] = ["boastful", "impulsive", "rebellious", "self-absorbed"]
 align_good: list[str] = ["compassionate", "helpful", "honest", "kind"]
 align_evil: list[str] = ["dishonest", "vengeful", "cruel", "greedy"]
 align_lawful: list[str] = ["cooperative", "loyal", "judgemental", "methodical"]
 align_neutral: list[str] = ["selfish", "disinterested", "laconic", "pragmatic"]
 
-ideal: list[str] = []
-personality: list[str] = ["alignment trait to fault"]
+virtues: list[str] = ["humility", "charity", "patience", "kindness", "chastity", "temperance", "diligence"]
 bond: list[str] = ["self", "familial", "organizational", "community", "region", "country"]
 flaw: list[str] = ["prideful", "greedy", "wrathful", "envious", "lustful", "gluttonous", "slothful"]
 
-family: list[str] = ["child", "children", "sister", "brother", "multiple siblings", ]
-mentor: list[str] = ["relative", "wandering stranger", "school", ""]
+family: list[str] = ["child", "children", "sister", "brother", "multiple siblings", "parent", "both parents"]
+mentor: list[str] = ["relative", "wandering stranger", "school", "family", "military"]
 mentor_attitude: list[str] = ["friendly", "neutral", "hostile"]
 
 
