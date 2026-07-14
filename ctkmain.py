9193c8b7a438d0b169b7fabe84aec82c3f607134
paxington181 <tsandrus@rogers.com>
2026-07-14 16:57:24 -0400

    Book selection working, just need to plumb the randomize function

--- ctkmain.py
+++ ctkmain.py
@@ -25,8 +25,42 @@ class char_roll(customtkinter.CTkFrame):
         self.randomize_button = customtkinter.CTkButton(self, text = "Roll Character", command = self.randomize)
         self.randomize_button.grid(row = 3, column = 0, columnspan = 5, padx = 10, sticky = "ew")
 
+        self.class_books = BooksFrame(self, "Class Books", books = ["2024 PHB", "Eberron"])
+        self.class_books.grid(row = 0, rowspan = 4, column = 6, columnspan =2, sticky = "ew")
+        self.subclass_books = BooksFrame(self, "SubClass Books", books = ["2024 PHB", "Eberron", "Ravenloft"])
+        self.subclass_books.grid(row = 0, rowspan = 4, column = 8, columnspan =2, padx = (10, 10), sticky = "ew")
+        self.species_books = BooksFrame(self, "Species Books", books = ["2024 PHB", "Eberron", "Ravenloft"])
+        self.species_books.grid(row = 0, rowspan = 4, column = 10, columnspan =2, padx = (10, 10), sticky = "ew")
+        self.background_books = BooksFrame(self, "Background Books", books = ["2024 PHB", "Eberron", "Ravenloft"])
+        self.background_books.grid(row = 0, rowspan = 4, column = 12, columnspan =2, padx = (10, 10), sticky = "ew")
+
     def randomize(self):
-        pass
+        print("Class: ", self.class_books.get())
+        print("Subclass: ", self.subclass_books.get())
+        print("Species: ", self.species_books.get())
+        print("Background: ", self.background_books.get())
+
+class BooksFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, books):
+        super().__init__(master)
+        self.title = title
+        self.books = books
+        self.checkboxes = []
+
+        self.title = customtkinter.CTkLabel(self, text = self.title, fg_color = "gray30", corner_radius = 6)
+        self.title.grid(row = 0, column = 0, columnspan = 2, padx = (10, 10), sticky = "nsew")
+
+        for i, books in enumerate(self.books):
+            checkbox = customtkinter.CTkCheckBox(self, text = books)
+            checkbox.grid(row = i + 1, column = 0, padx = 10, pady = (10, 0), sticky = "w")
+            self.checkboxes.append(checkbox)
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
 
 
 class App(customtkinter.CTk):
@@ -34,13 +68,15 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.title("D&D 2024 Random Character Generator")
-        self.geometry("900x425")
+        self.geometry("950x425")
         self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), weight = 1)
-        self.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), weight = 1)
+        self.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13), weight = 1)
         
         
         self.char_roll_frame = char_roll(self)
         self.char_roll_frame.grid(row = 0, column = 0, padx = 10, pady = 10, sticky = "nsew")
         
+   
+
 app = App()
 app.mainloop()
